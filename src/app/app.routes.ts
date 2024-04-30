d1226f3255d427825793f29ab94c85c7821a06ee
Gustav Brodin <brodin.gustav@gmail.com>
2024-04-30 15:16:01 +0200

    First Commit - ordnat med routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DiagramComponent } from './diagram/diagram.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "diagram", component: DiagramComponent},
+    {path: "", redirectTo: "diagram", pathMatch: "full"}
+];
