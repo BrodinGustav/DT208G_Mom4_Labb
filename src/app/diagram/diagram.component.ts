34388485f1ddfe45296cd885bd5745aa1aaf01a3
Gustav Brodin <brodin.gustav@gmail.com>
2024-05-01 14:15:51 +0200

    Commit 8 - Lagt in metod för sortering av tabelldata. Initierat egenskaper för metoden till klassen DiagramComponent. Lagt in key:string:any till interfacet för att lösa error "any" gällande valueA och B i sortTable metoden.  Justerat kommentarer och css

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-diagram',
   standalone: true,
-  imports: [CommonModule, FormsModule],                    //Import av CommonModule för användning av NgFor i diagram-component.html. Import av FormsModule för ngModule
+  imports: [CommonModule, FormsModule], //CommonModule för användning av NgFor i diagram-component.html. FormsModule för ngModule. SortableTableComponent för sortering av tabell.
   templateUrl: './diagram.component.html',
   styleUrl: './diagram.component.css'
 })
@@ -16,6 +16,8 @@ export class DiagramComponent {
   kurser: Kurser[] = [];                  //Array med kurser, importerar interface från models/kurser
   filtreradeKurser: Kurser[] = [];        //Ny array som lagrar kurser som filtreras utan att orginal arrayen (Kurser) raderas
   searchValue: string = "";               //searchValue kommer från ngModel i diagram.component.html
+  sortColumn: string = "";                //initierar egenskap för metod sortTable  
+  isAscending: boolean = true;            //initierar egenskap för metod sortTable
 
   constructor(private Ramschemaservice: RamschemaService ) {}  //Instans av Ramschemaservice injiceras i constructor
 
@@ -29,9 +31,24 @@ export class DiagramComponent {
   applyFilter(): void {                                       //Metod som filtrerar datan och lagrar i variablen filtreradeKurser
     this.filtreradeKurser = this.kurser.filter((kurser) => 
       kurser.code.toLowerCase().includes(this.searchValue.toLocaleLowerCase()) ||
-    kurser.coursename.toLowerCase().includes(this.searchValue.toLocaleLowerCase()) ||
-    kurser.progression.toLowerCase().includes(this.searchValue.toLocaleLowerCase()) 
-  );
+    kurser.coursename.toLowerCase().includes(this.searchValue.toLocaleLowerCase())
+      );
+  }
+
+  // Metod för att sortera tabellen
+  sortTable(column: string): void {
+    if (this.sortColumn === column) {         //Kontroll för att se om sortColumn är samma som användaren klickade på
+      this.isAscending = !this.isAscending;   //Om samma kolumn byts riktning på sortering
+    } else {
+      this.sortColumn = column;               
+      this.isAscending = true;
+    }
+
+    this.filtreradeKurser.sort((a, b) => {            //a, b representerar 2 element som jämförs för att bestämma stigande/fallande ordning
+      const valueA = a[column].toLowerCase();         //Hämtar värde för kolumen "a" och konverterar till små bokstäver 
+      const valueB = b[column].toLowerCase();
+      return this.isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);  //Jämför värde A och B för att se om ordningen ska sorteras stigande eller fallande.
+    });
   }
 
 }
--- src/app/models/kurser.ts
+++ src/app/models/kurser.ts
@@ -2,4 +2,5 @@ export interface Kurser {
     code: string,
     coursename: string,
     progression: string,
+    [key: string]: any;     //används för att bli av med "Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'Kurser'."  från diagrams ts-fil
 }
